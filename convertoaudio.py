58b69bef1fbf6b0de671d24a82bbbc87052ef239
Graham Heath <heathg@google.com>
2018-12-19 14:12:53 -0600

    Add SSML support to Polly. Convert tabs to spaces. Adhere to 80 character limits. Clean up indentation. Add utf-8 meta tag. Fix #voiceSelected option values. Add a default to search value.

--- convertoaudio.py
+++ convertoaudio.py
@@ -6,24 +6,24 @@ from boto3.dynamodb.conditions import Key, Attr
 def lambda_handler(event, context):
 
     postId = event["Records"][0]["Sns"]["Message"]
-    
+
     print "Text to Speech function. Post ID in DynamoDB: " + postId
-    
+
     #Retrieving information about the post from DynamoDB table
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(os.environ['DB_TABLE_NAME'])
     postItem = table.query(
         KeyConditionExpression=Key('id').eq(postId)
     )
-    
 
-    text = postItem["Items"][0]["text"]
-    voice = postItem["Items"][0]["voice"] 
-    
+
+    text = '<speak>' + postItem["Items"][0]["text"] + '</speak>'
+    voice = postItem["Items"][0]["voice"]
+
     rest = text
-    
-    #Because single invocation of the polly synthesize_speech api can 
-    # transform text with about 1,500 characters, we are dividing the 
+
+    #Because single invocation of the polly synthesize_speech api can
+    # transform text with about 1,500 characters, we are dividing the
     # post into blocks of approximately 1,000 characters.
     textBlocks = []
     while (len(rest) > 1100):
@@ -32,22 +32,23 @@ def lambda_handler(event, context):
 
         if (end == -1):
             end = rest.find(" ", 1000)
-            
+
         textBlock = rest[begin:end]
         rest = rest[end:]
         textBlocks.append(textBlock)
-    textBlocks.append(rest)            
+    textBlocks.append(rest)
 
     #For each block, invoke Polly API, which will transform text into audio
     polly = boto3.client('polly')
-    for textBlock in textBlocks: 
+    for textBlock in textBlocks:
         response = polly.synthesize_speech(
             OutputFormat='mp3',
             Text = textBlock,
-            VoiceId = voice
+            VoiceId = voice,
+            TextType = 'ssml',
         )
-        
-        #Save the audio stream returned by Amazon Polly on Lambda's temp 
+
+        #Save the audio stream returned by Amazon Polly on Lambda's temp
         # directory. If there are multiple text blocks, the audio stream
         # will be combined into a single file.
         if "AudioStream" in response:
@@ -59,21 +60,21 @@ def lambda_handler(event, context):
 
 
     s3 = boto3.client('s3')
-    s3.upload_file('/tmp/' + postId, 
-      os.environ['BUCKET_NAME'], 
+    s3.upload_file('/tmp/' + postId,
+      os.environ['BUCKET_NAME'],
       postId + ".mp3")
-    s3.put_object_acl(ACL='public-read', 
-      Bucket=os.environ['BUCKET_NAME'], 
+    s3.put_object_acl(ACL='public-read',
+      Bucket=os.environ['BUCKET_NAME'],
       Key= postId + ".mp3")
 
     location = s3.get_bucket_location(Bucket=os.environ['BUCKET_NAME'])
     region = location['LocationConstraint']
-    
+
     if region is None:
         url_begining = "https://s3.amazonaws.com/"
     else:
         url_begining = "https://s3-" + str(region) + ".amazonaws.com/" \
-    
+
     url = url_begining \
             + str(os.environ['BUCKET_NAME']) \
             + "/" \
@@ -84,11 +85,11 @@ def lambda_handler(event, context):
     response = table.update_item(
         Key={'id':postId},
           UpdateExpression=
-            "SET #statusAtt = :statusValue, #urlAtt = :urlValue",                   
+            "SET #statusAtt = :statusValue, #urlAtt = :urlValue",
           ExpressionAttributeValues=
             {':statusValue': 'UPDATED', ':urlValue': url},
         ExpressionAttributeNames=
           {'#statusAtt': 'status', '#urlAtt': 'url'},
     )
-        
+
     return
